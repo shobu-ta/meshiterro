7562b05601eef403cdb74c864da7a1785f89abb8
shobu-ta <196527230+shobu-ta@users.noreply.github.com>
2025-03-02 09:41:59 +0000

    chapter15 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-
   devise_for :users
   root to: "homes#top"
 
   # 名前付きルート（パス名）をaboutに設定するための記述
   get 'homes/about', to: 'homes#about', as: 'about'
   # resourcesはpost_imagesリソース用の特定のルートを定義する簡潔な方法でonlyで利用可能なアクションをnew、index、showに限定している
-  resources :post_images, only: [:new, :create, :index, :show]
-
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
