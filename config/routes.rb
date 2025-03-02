25b1eef1e0e8d88fdb0cbe9678fd75cbcc94262b
shobu-ta <196527230+shobu-ta@users.noreply.github.com>
2025-03-02 13:54:26 +0000

    chapter18 complete

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # 名前付きルート（パス名）をaboutに設定するための記述
   get 'homes/about', to: 'homes#about', as: 'about'
   # resourcesはpost_imagesリソース用の特定のルートを定義する簡潔な方法でonlyで利用可能なアクションをnew、index、showに限定している
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
